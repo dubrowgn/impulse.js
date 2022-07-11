f02f8d4fc0134cc0e988310eb91c82412b902622
Dustin Brown <me@dubrowgn.com>
2022-07-11 10:19:57 -0600

    Add AudioCache

---
+++ src/asset/audio-cache.ts
@@ -0,0 +1,19 @@
+export class AudioCache {
+	protected cache: Map<string, HTMLAudioElement> = new Map();
+
+	get(path: string) {
+		let audio = this.cache.get(path);
+		if (audio !== undefined)
+			return audio;
+
+		audio = new Audio(path);
+
+		this.cache.set(path, audio);
+
+		return audio;
+	}
+
+	clear() {
+		this.cache.clear();
+	}
+};
