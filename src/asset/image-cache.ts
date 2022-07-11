645513a68141d20fc947a6cb11d65c1cb22c3c7a
Dustin Brown <me@dubrowgn.com>
2022-07-11 10:19:47 -0600

    Add ImageCache

---
+++ src/asset/image-cache.ts
@@ -0,0 +1,20 @@
+export class ImageCache {
+	protected cache: Map<string, HTMLImageElement> = new Map();
+
+	get(path: string) {
+		let img = this.cache.get(path);
+		if (img !== undefined)
+			return img;
+
+		img = new Image();
+		img.src = path;
+
+		this.cache.set(path, img);
+
+		return img;
+	}
+
+	clear() {
+		this.cache.clear();
+	}
+};
